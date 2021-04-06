a15972beb56e95bfc64355adea073fcde51961be
戴凌风 <dtjqh5252@hotmail.com>
2021-04-06 17:28:39 +0800

    首次进入基本完成

--- pages/login/login.js
+++ pages/login/login.js
@@ -13,11 +13,10 @@ userInfo: {}
     wx.getUserProfile({
       desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
-        // this.setData({
-        //   userInfo: res.userInfo,
-        //   hasUserInfo: true
-        // })
         wx.setStorageSync('userInfo', res.userInfo)
+        wx.navigateTo({
+          url: 'userInfo',
+        })
       }
     })
   },
--- pages/login/userInfo.js
+++ pages/login/userInfo.js
@@ -5,9 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    index: -1,
+    realName:'',
+    array: [{
+        id: 0,
+        subject: "语文"
+      },
+      {
+        id: 1,
+        subject: "数学"
+      },
+      {
+        id: 2,
+        subject: "英语"
+      },
+      {
+        id: 3,
+        subject: "美术"
+      },
+    ]
   },
-
+  bindPickerChange: function (e) {
+    wx.hideKeyboard()
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindKeyInput: function(e){
+    this.setData({
+      realName: e.detail.value
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
