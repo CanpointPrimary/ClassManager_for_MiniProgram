f6573c2d67de779b7252efd65c965ba64761b289
戴凌风 <dtjqh5252@hotmail.com>
2021-04-09 14:50:23 +0800

    fix: fix requset use to much result in the menu is blank for a long time

--- pages/homePage/homePage.js
+++ pages/homePage/homePage.js
@@ -35,7 +35,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad:  function (options) {
     wx.request({
       url: 'http://canpoint/api/menu',
       success: ({
@@ -45,6 +45,7 @@ Page({
           menuList: data.menu
         })
       },
+      timeout: 500,
       fail: () => {
         this.setData({
           menuList: [{
