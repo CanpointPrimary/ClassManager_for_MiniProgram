17a5f45a271cddeacbdae1006c3ec657793a66d0
Lingfeng <dtjqh5252@hotmail.com>
2021-05-08 15:10:59 +0800

    refactor: refactor the tab-bar component
    optimize its display on iPhoneX

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -11,22 +11,26 @@ Component({
       iconPath: "/static/icon/tab_shouye_nor.svg",
       selectedIconPath: "/static/icon/tab_shouye_sel.svg"
     }, {
+      pagePath: "/pages/classStatus/classStatus",
+      text: "学情",
+      iconPath: "/static/icon/tab_xueqing-nor.svg",
+      selectedIconPath: "/static/icon/tab_xueqing_sel.svg"
+    }, {
       pagePath: "/pages/users/users",
       text: "个人中心",
       iconPath: "/static/icon/tab_zhongxin_nor.svg",
       selectedIconPath: "/static/icon/tab_zhongxin_sel.svg"
     }]
   },
-  attached() {},
+  attached() {
+    console.log(this.data.selected);
+  },
   methods: {
     switchTab(e) {
       const data = e.currentTarget.dataset
       const url = data.path
       wx.switchTab({
         url
-      })
-      this.setData({
-        selected: data.index
       })
     }
   }
