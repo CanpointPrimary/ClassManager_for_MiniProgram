d7ee03bb37f79b6d4e9408312c65071d9e1aeb9c
Lingfeng <dtjqh5252@hotmail.com>
2021-05-12 09:15:40 +0800

    refactor: change the irregular name of icons

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -8,18 +8,18 @@ Component({
     list: [{
       pagePath: "/pages/homePage/homePage",
       text: "首页",
-      iconPath: "/static/icon/tab_shouye_nor.svg",
-      selectedIconPath: "/static/icon/tab_shouye_sel.svg"
+      iconPath: "/static/icon/home.svg",
+      selectedIconPath: "/static/icon/actHome.svg"
     }, {
       pagePath: "/pages/classStatus/classStatus",
       text: "学情",
-      iconPath: "/static/icon/tab_xueqing-nor.svg",
-      selectedIconPath: "/static/icon/tab_xueqing_sel.svg"
+      iconPath: "/static/icon/classStatus.svg",
+      selectedIconPath: "/static/icon/actClassStatus.svg"
     }, {
       pagePath: "/pages/users/users",
       text: "个人中心",
-      iconPath: "/static/icon/tab_zhongxin_nor.svg",
-      selectedIconPath: "/static/icon/tab_zhongxin_sel.svg"
+      iconPath: "/static/icon/user.svg",
+      selectedIconPath: "/static/icon/actUser.svg"
     }]
   },
   attached() {},
