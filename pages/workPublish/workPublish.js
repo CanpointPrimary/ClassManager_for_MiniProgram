85dae7257e16d3708d28151c880c5c0b2bcbb881
Lingfeng <dtjqh5252@hotmail.com>
2021-06-04 16:01:18 +0800

    feat: add time picker for endTime

--- pages/workPublish/workPublish.js
+++ pages/workPublish/workPublish.js
@@ -7,7 +7,11 @@ Page({
     data: {
         recipient: "1年级2班",
         title: "2021.3.11（周三）英语作业",
-        workPack: [{}, {}]
+        workPack: [{}, {}],
+        array: [1, 2, 3, 4, 5, 6],
+        showTimePicker: true,
+        timeSelect: [],
+        timeList: []
     },
     titleChange(e) {
         this.setData({
@@ -18,9 +22,49 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        let dateList = []
+        const day = ["星期天", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
+        const date = new Date()
+        let today = date.getDate()
+        let month
+        let theMonth = date.getMonth()
+        let tDate
+        let dateStr
+        let hour
+        let minutes
+        let timeList = []
+        for (let i = 0; i < 30; i++) {
+            date.setMonth(theMonth)
+            date.setDate(today + i)
+            if (date.getMonth() + 1 < 10) {
+                month = '0' + (date.getMonth() + 1)
+            }
+            if (date.getDate() < 10) {
+                tDate = '0' + date.getDate()
+            } else {
+                tDate = date.getDate()
+            }
+            dateStr = `${month}/${tDate} ${day[date.getDay()]}`
+            dateList.push(dateStr)
+        }
+        for (let i = 0; i < 24; i++) {
+            hour = i < 10 ? `0${i}` : `${i}`;
+            for (let j = 0; j < 2; j++) {
+                minutes = j ? '30' : '00';
+                timeList.push(`${hour}:${minutes}`)
+            }
+        }
 
+        this.setData({
+            timeList: [
+                ['默认', ...dateList], timeList
+            ],
+            timeSelect: [0, date.getHours() * 2 + 2]
+        })
     },
-
+    timeChange(e) {
+        console.log(e.detail.value);
+    },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
