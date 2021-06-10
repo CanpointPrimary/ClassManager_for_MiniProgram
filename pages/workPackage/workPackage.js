ac7dee22ff0e0f6542d4d1b777c62fe748794995
Lingfeng <dtjqh5252@hotmail.com>
2021-06-10 11:40:53 +0800

    feat: add work navigation

--- pages/workPackage/workPackage.js
+++ pages/workPackage/workPackage.js
@@ -5,14 +5,45 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        workPackageInfo: {
+            title: '2021.3.11（周三）英语作业',
+            subject: "英语",
+            publisher: "巫美美",
+            pubTime: "3.11 17:03",
+            endTime: "3.12 17:03",
+            desc: '把今天家庭作业做了以后，认真订正。'
+        },
+        currentWork: 0,
+        workList: [{
+            type: '跟读',
+            title: 'unit 2单元',
+            timeTake: "4分钟"
+        },{
+            type: '跟读',
+            title: 'unit 2单元',
+            timeTake: "4分钟"
+        },{
+            type: '跟读',
+            title: 'unit 2单元',
+            timeTake: "4分钟"
+        },{
+            type: '跟读',
+            title: 'unit 2单元',
+            timeTake: "4分钟"
+        },{
+            type: '跟读',
+            title: 'unit 2单元',
+            timeTake: "4分钟"
+        }, ]
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        wx.setNavigationBarTitle({
+            title: this.data.workPackageInfo.title
+        })
     },
 
     /**
