4fc806b291d9d6eada698c0165c9dd7ea022f6b7
Lingfeng <dtjqh5252@hotmail.com>
2021-06-22 15:23:16 +0800

    refactor: make mock data a javascript document

---
+++ utils/mockData.js
@@ -0,0 +1,234 @@
+let answers = [{
+  id: 0,
+  rname: '李梅',
+  avatar: '/static/avatar.png',
+  subTime: '2021.3.12 17:03',
+  works: [{
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'B'
+  }, {
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'A'
+  }],
+  useTime: '7分钟',
+  tag: '进步明显',
+  comments: [{
+    from: '巫老师',
+    val: '希望再接再厉，今天完成得很好！',
+    voice: ''
+  }]
+}, {
+  id: 1,
+  rname: '李梅',
+  avatar: '/static/avatar.png',
+  subTime: '2021.3.12 17:03',
+  works: [{
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'B'
+  }, {
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'A'
+  }],
+  useTime: '7分钟',
+  tag: '',
+  comments: []
+}, {
+  id: 2,
+  rname: '李梅',
+  avatar: '/static/avatar.png',
+  subTime: '2021.3.12 17:03',
+  works: [{
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'B'
+  }, {
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'A'
+  }],
+  useTime: '7分钟',
+  tag: '进步明显',
+  comments: [{
+    from: '巫老师',
+    val: '希望再接再厉，今天完成得很好！',
+    voice: ''
+  }, {
+    from: '巫老师',
+    val: '',
+    voice: '/static/river.mp3 '
+  }]
+}, {
+  id: 3,
+  rname: '李梅',
+  avatar: '/static/avatar.png',
+  subTime: '2021.3.12 17:03',
+  works: [{
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'B'
+  }, {
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'A'
+  }],
+  useTime: '7分钟',
+  tag: '',
+  comments: [{
+    from: '巫老师',
+    val: '',
+    voice: '/static/river.mp3 '
+  }]
+}, {
+  id: 4,
+  rname: '李梅',
+  avatar: '/static/avatar.png',
+  subTime: '2021.3.12 17:03',
+  works: [{
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'B'
+  }, {
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'A'
+  }],
+  useTime: '7分钟',
+  tag: '',
+  comments: []
+}, {
+  id: 5,
+  rname: '李梅',
+  avatar: '/static/avatar.png',
+  subTime: '2021.3.12 17:03',
+  works: [{
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'B'
+  }, {
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'A'
+  }],
+  useTime: '7分钟',
+  tag: '进步明显',
+  comments: []
+}, {
+  id: 6,
+  rname: '李梅',
+  avatar: '/static/avatar.png',
+  subTime: '2021.3.12 17:03',
+  works: [{
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'B'
+  }, {
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'A'
+  }],
+  useTime: '7分钟',
+  tag: '进步明显',
+  comments: [{
+    from: '巫老师',
+    val: '希望再接再厉，今天完成得很好！',
+    voice: ''
+  }, {
+    from: '巫老师',
+    val: '',
+    voice: '/static/river.mp3 '
+  }]
+}, {
+  id: 7,
+  rname: '李梅',
+  avatar: '/static/avatar.png',
+  subTime: '2021.3.12 17:03',
+  works: [{
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'B'
+  }, {
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'A'
+  }],
+  useTime: '7分钟',
+  tag: '进步明显',
+  comments: [{
+    from: '巫老师',
+    val: '希望再接再厉，今天完成得很好！',
+    voice: ''
+  }]
+}, {
+  id: 8,
+  rname: '李梅',
+  avatar: '/static/avatar.png',
+  subTime: '2021.3.12 17:03',
+  works: [{
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'B'
+  }, {
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'A'
+  }],
+  useTime: '7分钟',
+  tag: '进步明显',
+  comments: [{
+    from: '巫老师',
+    val: '希望再接再厉，今天完成得很好！',
+    voice: ''
+  }, {
+    from: '巫老师',
+    val: '',
+    voice: '/static/river.mp3 '
+  }]
+}, {
+  id: 9,
+  rname: '李梅',
+  avatar: '/static/avatar.png',
+  subTime: '2021.3.12 17:03',
+  works: [{
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'B'
+  }, {
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'A'
+  }],
+  useTime: '7分钟',
+  comments: [{
+    from: '巫老师',
+    val: '希望再接再厉，今天完成得很好！',
+    voice: ''
+  }]
+}, {
+  id: 10,
+  rname: '李梅',
+  avatar: '/static/avatar.png',
+  subTime: '2021.3.12 17:03',
+  works: [{
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'B'
+  }, {
+    type: '跟读',
+    title: 'unit 2单元',
+    socre: 'A'
+  }],
+  useTime: '7分钟',
+  tag: '进步明显',
+  comments: [{
+    from: '巫老师',
+    val: '希望再接再厉，今天完成得很好！',
+    voice: ''
+  }]
+}]
+module.exports = {
+  answers
+}
