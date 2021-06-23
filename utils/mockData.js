6ae9f8d7f87afe997dac0901723bf132124eb287
Lingfeng <dtjqh5252@hotmail.com>
2021-06-23 13:32:45 +0800

    feat: add name of student that whether have submited homework list

--- utils/mockData.js
+++ utils/mockData.js
@@ -229,6 +229,165 @@ let answers = [{
     voice: ''
   }]
 }]
+
+// 作业提交名单
+let subStatusList = {
+  notViewed: [{
+      id: 0,
+      rname: "于秀英"
+    },
+    {
+      id: 1,
+      rname: "蒋霞"
+    },
+    {
+      id: 2,
+      rname: "武芳"
+    },
+    {
+      id: 3,
+      rname: "雷刚"
+    },
+    {
+      id: 4,
+      rname: "吕桂英"
+    },
+    {
+      id: 5,
+      rname: "于磊"
+    },
+    {
+      id: 6,
+      rname: "胡娜"
+    },
+    {
+      id: 7,
+      rname: "史娟"
+    },
+    {
+      id: 8,
+      rname: "范敏"
+    },
+    {
+      id: 9,
+      rname: "蒋艳"
+    },
+    {
+      id: 10,
+      rname: "欧阳敬城"
+    },
+  ],
+  notSubmit: [{
+      id: 11,
+      rname: "姜艳"
+    },
+    {
+      id: 12,
+      rname: "胡霞"
+    },
+    {
+      id: 13,
+      rname: "陆军"
+    },
+    {
+      id: 14,
+      rname: "宋静"
+    },
+    {
+      id: 15,
+      rname: "于平"
+    },
+    {
+      id: 16,
+      rname: "万磊"
+    },
+    {
+      id: 17,
+      rname: "邓刚"
+    },
+    {
+      id: 18,
+      rname: "程强"
+    },
+    {
+      id: 19,
+      rname: "彭平"
+    }
+  ],
+  hadSubmit: [{
+      id: 20,
+      rname: "郑杰"
+    },
+    {
+      id: 21,
+      rname: "林殊"
+    }, {
+      id: 22,
+      rname: "林涛"
+    },
+    {
+      id: 23,
+      rname: "胡娜"
+    },
+    {
+      id: 24,
+      rname: "赵静"
+    },
+    {
+      id: 25,
+      rname: "易平"
+    },
+    {
+      id: 26,
+      rname: "廖秀兰"
+    },
+    {
+      id: 27,
+      rname: "秦刚"
+    },
+    {
+      id: 28,
+      rname: "郭超"
+    },
+    {
+      id: 29,
+      rname: "赵桂英"
+    },
+    {
+      id: 30,
+      rname: "赵娟"
+    },
+    {
+      id: 31,
+      rname: "李丽"
+    },
+    {
+      id: 32,
+      rname: "董明"
+    },
+    {
+      id: 33,
+      rname: "周艳"
+    },
+    {
+      id: 34,
+      rname: "汪艳"
+    },
+    {
+      id: 35,
+      rname: "江桂英"
+    },
+    {
+      id: 36,
+      rname: "戴勇"
+    },
+    {
+      id: 37,
+      rname: "孔丽"
+    }
+  ]
+}
 module.exports = {
-  answers
+  answers,
+  subStatusList
 }
