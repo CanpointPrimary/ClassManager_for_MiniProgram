83b6deb066f890d1e7bc41761c7430d55d666800
Lingfeng <dtjqh5252@hotmail.com>
2021-07-23 14:54:01 +0800

    refactor: refactor the joinclass page

--- pages/joinClass/joinClass.js
+++ pages/joinClass/joinClass.js
@@ -50,7 +50,7 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        let invited = true
+        let invited = false
         let index = 0
         if (invited) index = 1
         this.setData({
