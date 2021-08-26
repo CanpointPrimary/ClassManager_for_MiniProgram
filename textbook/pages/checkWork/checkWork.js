342dccb9616e75613dfb7acbbf2048756004d593
Lingfeng <dtjqh5252@hotmail.com>
2021-08-26 11:20:15 +0800

    feat: build checkwork page

--- textbook/pages/checkWork/checkWork.js
+++ textbook/pages/checkWork/checkWork.js
@@ -5,14 +5,14 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        score: ["已阅", "A", "B", "C", "D", 'E'],
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        console.log(wx.getSystemInfoSync());
     },
 
     /**
