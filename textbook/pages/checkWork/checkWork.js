e7a9af513c8e241a7482c4ad49d5bebc6cb74358
Lingfeng <dtjqh5252@hotmail.com>
2021-08-27 16:00:36 +0800

    feat: add the canvas picture

--- textbook/pages/checkWork/checkWork.js
+++ textbook/pages/checkWork/checkWork.js
@@ -12,15 +12,40 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        console.log(wx.getSystemInfoSync());
+        wx.createSelectorQuery().select('#work').fields({
+            node: true,
+            size: true,
+        }).exec((res) => {
+            const canvas = res[0].node
+            const ctx = res[0].node.getContext('2d')
+            const width = res[0].width
+            const height = res[0].height
+            const dpr = wx.getSystemInfoSync().pixelRatio
+            res[0].node.width = width * dpr
+            res[0].node.height = height * dpr
+            let img = canvas.createImage()
+            wx.downloadFile({
+                url: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fnimg.ws.126.net%2F%3Furl%3Dhttp%253A%252F%252Fdingyue.ws.126.net%252F2021%252F0824%252F1c916f76j00qycd4l003uc000h600m8c.jpg%26thumbnail%3D650x2147483647%26quality%3D80%26type%3Djpg&refer=http%3A%2F%2Fnimg.ws.126.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1632559210&t=54349fe8646c8de64d3b2ae8901fb38b',
+                success: (res) => {
+                    this._src = res.tempFilePath
+                    img.src = res.tempFilePath
+                },
+                fail: (err) => {
+                    console.log(err);
+                }
+            })
+            img.onload = () => {
+                ctx.scale(width / img.width, width / img.width)
+                ctx.drawImage(img, 0, 0, img.width * dpr, img.height * dpr)
+            }
+        })
+
     },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function () {
-
-    },
+    onReady: function () {},
 
     /**
      * 生命周期函数--监听页面显示
